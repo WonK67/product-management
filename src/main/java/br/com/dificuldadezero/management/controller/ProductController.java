de02248c104d3ea7a38dfc571b761273ce1d6cc2
Eduardo Campos Dias <eduardo.campos.dias@usp.br>
2018-11-02 17:46:04 -0300

    adicionado metodo provisorio enquanto banco de dados nao funciona

--- src/main/java/br/com/dificuldadezero/management/controller/ProductController.java
+++ src/main/java/br/com/dificuldadezero/management/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.dificuldadezero.management.controller;
 
 import br.com.dificuldadezero.management.dao.ProductHibernateDAO;
 import br.com.dificuldadezero.management.dto.Product;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,37 +16,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    @GetMapping(value="/produto")
-    public ResponseEntity<List<Product>> getAllProducts(){
-        ProductHibernateDAO productDAO = ProductHibernateDAO.getInstance();
-        return new ResponseEntity<>(productDAO.findAll(), HttpStatus.OK);
+
+    private ArrayList<Product> temporaryProductList = new ArrayList<>();
+
+    @GetMapping(value = "/produto")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        /*ProductHibernateDAO productDAO = ProductHibernateDAO.getInstance();
+        return new ResponseEntity<>(productDAO.findAll(), HttpStatus.OK);*/
+        return new ResponseEntity<>(temporaryProductList, HttpStatus.OK);
     }
-    
-    @GetMapping(value="/produto/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable long id){
-        ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
+
+    @GetMapping(value = "/produto/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable long id) {
+        /*ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
         Product product = dao.getById(id);
-        return new ResponseEntity<>(product, HttpStatus.OK);
+        return new ResponseEntity<>(product, HttpStatus.OK);*/
+        return new ResponseEntity<>(temporaryProductList.get((int) id), HttpStatus.OK);
     }
-    
-    @PostMapping(value="/produto")
-    public ResponseEntity<Product> postProduct(@RequestBody Product product){
-        ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
+
+    @PostMapping(value = "/produto")
+    public ResponseEntity<Product> postProduct(@RequestBody Product product) {
+        /*ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
         dao.persist(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);*/
+        temporaryProductList.add(product);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
-    
-    @PutMapping(value="/produto")
-    public ResponseEntity<Product> putProduct(@RequestBody Product product){
-        ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
+
+    @PutMapping(value = "/produto")
+    public ResponseEntity<Product> putProduct(@RequestBody Product product) {
+        /*ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
         dao.merge(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);*/
+        for (int i = 0; i < temporaryProductList.size(); i++) {
+            if (temporaryProductList.get(i).getId_produto() == product.getId_produto()) {
+                temporaryProductList.remove(i);
+                temporaryProductList.add(product);
+            }
+        }
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
-    
-    @DeleteMapping(value="/produto/{id}")
-    public ResponseEntity<Product> deleteProduct(@PathVariable long id){
-        ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
+
+    @DeleteMapping(value = "/produto/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
+        /*ProductHibernateDAO dao = ProductHibernateDAO.getInstance();
         dao.removeById(id);
+        return new ResponseEntity<>(HttpStatus.OK);*/
+        for (int i = 0; i < temporaryProductList.size(); i++) {
+            if (temporaryProductList.get(i).getId_produto() == id) {
+                temporaryProductList.remove(i);
+            }
+        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
